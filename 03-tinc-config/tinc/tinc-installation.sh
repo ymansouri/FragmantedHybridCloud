f94b9a70380bb3eea803931aad3a188e1c8b7133
shagun971 <61769787+shagun971@users.noreply.github.com>
2023-05-11 20:12:43 +0930

    Update tinc-installation.sh

--- 03-tinc-config/tinc/tinc-installation.sh
+++ 03-tinc-config/tinc/tinc-installation.sh
@@ -10,16 +10,12 @@ grep "${LINE_TO_APPEND}" /etc/hosts > /dev/null
 #sudo apt update -y
 #sudo apt -y install tinc
 
-
-
 # Step 1: tinc 1.1  installation  using  Debian package
 sudo apt update -y
 #dependency
 sudo apt-get install libminiupnpc17
-wget http://http.us.debian.org/debian/pool/main/t/tinc/tinc_1.1~pre18-1_amd64.deb
+#wget http://http.us.debian.org/debian/pool/main/t/tinc/tinc_1.1~pre18-1_amd64.deb
 sudo dpkg --install tinc_1.1~pre18-1_amd64.deb
-
-
 
 #tinc installation check 
 #tinc --version
